1bef0ec4a85bf60abbb6f401aa9dec0784e99dda
Stefan Caky <stefan.caky777@gmail.com>
2021-01-28 21:50:18 +0000

    Second part
    Finished 4 listeners

--- js/script.js
+++ js/script.js
@@ -5,15 +5,28 @@ const otherJobRole = document.querySelector('#other-job-role');
 const color = document.querySelector('#color');
 const design = document.querySelector('#design');
 const activitiesBox = document.querySelector('#activities-box').childNodes;
+const activitiesCost = document.querySelector('#activities-cost');
+const formElement = document.querySelector('.container').lastElementChild;
+const payPal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+const payment = document.querySelector('#payment');
+const creditCard = document.querySelector('#credit-card');
 
+let totalCost = 0;
 // set user input focus tu true - When the page first loads, the first text field  will focus state by default to prompt the user.
 userName.focus();
 // Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.
 otherJobRole.style.display = 'none';
 
+display(paypal, 'none');
+display(bitcoin, 'none');
+
+
 color.disabled = true;
 
-
+function display (element, display) {
+    element.style.display = display;
+}
 
 
 /**
@@ -30,7 +43,7 @@ jobRole.addEventListener('change', (e) => {
     
   
     if(e.target.value === other.value) {
-        otherJobRole.style.display = 'block';
+        display(otherJobRole, 'block');
     }
 });
 
@@ -54,6 +67,7 @@ design.addEventListener('change', (e) => {
           
         let choosedColor = color.options[i].getAttribute('data-theme');
         if (e.target.value == choosedColor) {
+            
             color.options[i].style.display = 'block';
             color.options[i].selected = true;
         } else {
@@ -77,15 +91,68 @@ design.addEventListener('change', (e) => {
 
 
 
-//  for(let i = 1; activitiesBox.length -1 > i; i++){
-//      let activitiesCheckBox = activitiesBox[i].firstElementChild;
+ for(let i = 1; activitiesBox.length -1 > i; i++){
+    
+    
+     activitiesBox[i].addEventListener('change', (e) => {
+         let choosedCost = parseInt(e.target.getAttribute('data-cost'));
+        let inputCheckBox = activitiesBox[i].firstElementChild
+         if(inputCheckBox.checked) {
+            totalCost += choosedCost;
+         } else {
+            totalCost -= choosedCost;
+             
+           
+         }
+         
 
-//      console.log(activitiesCheckBox)
-//       activitiesCheckBox.addEventListener('change', (e) => {
-//            // console.log(e.target.getAttribute('data-cost'))
-//       });
-//  }
-//
+         activitiesCost.innerHTML = `Total: $${totalCost}`;
+
+         
+      });
+ }
+
+
+/**
+ * This is a function.
+ *
+ * @param {string} n - A string param
+ * @return {string} A good string
+ *
+ * @example
+ *
+ *     foo('hello')
+ */
+payment.addEventListener('change', (e) => {
+    let choosedMethod = e.target.value;
+    display(bitcoin, 'none');
+    display(payPal, 'none');
+    if(choosedMethod === 'paypal') {
+        display(payPal, 'block');
+        display(creditCard, 'none');
+    } else if (choosedMethod === 'bitcoin') {
+        display(bitcoin, 'block');
+        display(creditCard, 'none');
+    } else {
+        display(creditCard, 'block');
+    }
+})
+
+/**
+ * This is a function.
+ *
+ * @param {string} n - A string param
+ * @return {string} A good string
+ *
+ * @example
+ *
+ *     foo('hello')
+ */
+
+formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('clicked')
+})
 
 
 
