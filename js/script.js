6199839d806bebf9b44bf735d7d23a16f6af4fcd
Stefan Caky <stefan.caky777@gmail.com>
2021-01-30 18:37:25 +0000

    changed functions
    changed functions

--- js/script.js
+++ js/script.js
@@ -1,158 +1,127 @@
 const userName = document.querySelector('#name');
+const email = document.querySelector('#email');
 const jobRole = document.querySelector('#title');
 const other = jobRole.lastElementChild;
 const otherJobRole = document.querySelector('#other-job-role');
 const color = document.querySelector('#color');
 const design = document.querySelector('#design');
-const activitiesBox = document.querySelector('#activities-box').childNodes;
+const activitiesBox = document.querySelector('#activities-box');
+const activities = document.querySelectorAll('label > input[type="checkbox"]');
 const activitiesCost = document.querySelector('#activities-cost');
 const formElement = document.querySelector('.container').lastElementChild;
 const payPal = document.querySelector('#paypal');
 const bitcoin = document.querySelector('#bitcoin');
 const payment = document.querySelector('#payment');
 const creditCard = document.querySelector('#credit-card');
+const ccNum = document.querySelector('#cc-num');
+const zip = document.querySelector('#zip');
+const cvv = document.querySelector('#cvv');
 
 let totalCost = 0;
-// set user input focus tu true - When the page first loads, the first text field  will focus state by default to prompt the user.
-userName.focus();
-// Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.
-otherJobRole.style.display = 'none';
 
-display(paypal, 'none');
-display(bitcoin, 'none');
+// HELPER FUNCTIONS _START
 
-
-color.disabled = true;
-
-function display (element, display) {
-    element.style.display = display;
+// display function show or hide, also working in loops
+function display (element, display, i) {
+   
+    if(i) {
+        element.options[i].style.display = display; 
+    } else {
+        element.style.display = display;
+    }
 }
 
-
-/**
- * This is a function.
- *
- * @param {string} n - A string param
- * @return {string} A good string
- *
- * @example
- *
- *     foo('hello')
- */
-jobRole.addEventListener('change', (e) => {
-    
-  
-    if(e.target.value === other.value) {
-        display(otherJobRole, 'block');
+// check if required fields are valid formation.. if yer return true if not return false
+const check =  {
+    username(username) {
+        return /^[\w\d]+/.test(username);
+    },
+    email(email) {
+        return /[^@]+@[^@.]+\.[a-z]+/i.test(email);
+    },
+    ccNum(ccNum) {
+        return  /^\d{13,16}$/.test(parseInt(ccNum));
+    },
+    zip(zip) {
+        return /^\d{5}$/.test(parseInt(zip));
+    },
+    cvv(cvv){
+        return /^\d{3}$/.test(parseInt(cvv));
     }
-});
+}
 
-/**
- * This is a function.
- *
- * @param {string} n - A string param
- * @return {string} A good string
- *
- * @example
- *
- *     foo('hello')
- */
+// HELPER FUNCTIONS _END
 
-console.log(color.options[4].getAttribute('data-theme'));
+// set the elements for first page load
+userName.focus();
+display(otherJobRole, 'none');
+color.disabled = true;
+payment.options[1].selected = true;
+display(payPal, 'none');
+display(bitcoin, 'none');
 
-design.addEventListener('change', (e) => {
-    color.disabled = false;
- 
-      for(let i = 1; i < color.options.length; i++) {
-          
+// job role function to hide or show input
+const jobRoleListener = (e) => {
+    let input = e.target.value;
+    
+    if(input == 'other') {
+        display(otherJobRole, 'block');
+    } else {
+        display(otherJobRole, 'none');
+    }
+}
+
+// enable color selection and show available color by design input
+const designListener = (e) => {
+    let input = e.target.value;
+    input ? color.disabled = false : '';
+
+    for(let i = 1; color.options.length > i; i++) {
         let choosedColor = color.options[i].getAttribute('data-theme');
-        if (e.target.value == choosedColor) {
-            
-            color.options[i].style.display = 'block';
+        console.log(choosedColor);
+
+        if(input == choosedColor) {
+            display(color, 'block', i);
             color.options[i].selected = true;
         } else {
-            color.options[i].style.display = 'none';
+            display(color, 'none', i);
             color.options[i].selected = false;
         }
-      }       
+    }
+}
+
+const activitiesBoxListener = (e) => {
+    let input = e.target;
+    let price = input.getAttribute('data-cost');
     
-});
+    price = parseInt(price);
+    input.checked ? totalCost += price : totalCost -= price;
+   
+    activitiesCost.innerHTML = `Total: $${totalCost}`;
+}
 
-/**
- * This is a function.
- *
- * @param {string} n - A string param
- * @return {string} A good string
- *
- * @example
- *
- *     foo('hello')
- */
-
-
-
- for(let i = 1; activitiesBox.length -1 > i; i++){
-    
-    
-     activitiesBox[i].addEventListener('change', (e) => {
-         let choosedCost = parseInt(e.target.getAttribute('data-cost'));
-        let inputCheckBox = activitiesBox[i].firstElementChild
-         if(inputCheckBox.checked) {
-            totalCost += choosedCost;
-         } else {
-            totalCost -= choosedCost;
-             
-           
-         }
-         
-
-         activitiesCost.innerHTML = `Total: $${totalCost}`;
-
-         
-      });
- }
-
-
-/**
- * This is a function.
- *
- * @param {string} n - A string param
- * @return {string} A good string
- *
- * @example
- *
- *     foo('hello')
- */
-payment.addEventListener('change', (e) => {
-    let choosedMethod = e.target.value;
+const paymentListener = (e) => {
+    let value = e.target.value;
     display(bitcoin, 'none');
     display(payPal, 'none');
-    if(choosedMethod === 'paypal') {
-        display(payPal, 'block');
-        display(creditCard, 'none');
-    } else if (choosedMethod === 'bitcoin') {
+    display(creditCard, 'none');
+
+   if(value === 'bitcoin') {
         display(bitcoin, 'block');
-        display(creditCard, 'none');
-    } else {
+   } else if (value == 'paypal') {
+        display(payPal, 'block');
+   } else {
         display(creditCard, 'block');
-    }
-})
+   }
+}
 
-/**
- * This is a function.
- *
- * @param {string} n - A string param
- * @return {string} A good string
- *
- * @example
- *
- *     foo('hello')
- */
+jobRole.addEventListener('change', jobRoleListener);
 
-formElement.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('clicked')
-})
+design.addEventListener('change', designListener);
+
+activitiesBox.addEventListener('change', activitiesBoxListener);
+
+payment.addEventListener('change', paymentListener);
 
 
 
